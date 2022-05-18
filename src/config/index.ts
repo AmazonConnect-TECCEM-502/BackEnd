0c81558d3df5aaffe2a7710a3aa25fbfc58babea
a01750264 <a01750246@itesm.mx>
2022-05-18 17:50:34 -0500

    GET and POST Call to S3

--- src/config/index.ts
+++ src/config/index.ts
@@ -15,11 +15,22 @@ export const DB_PASSWORD = process.env.DB_PASSWORD
 export const DB_HOST = process.env.DB_HOST
   ? process.env.DB_HOST
   : "aws-capstone-gp2.cjkp3k2seo8m.us-east-1.rds.amazonaws.com";
-  export const AWS_REGION = process.env.AWS_REGION 
-  ? process.env.AWS_REGION : "us-east-1";
-  export const AWS_ACCESS_KEY = process.env.AWS_ACCESS_KEY 
-  ? process.env.AWS_ACCESS_KEY : "";
-  export const AWS_SECRET_ACCESS_KEY = process.env.AWS_SECRET_ACCESS_KEY 
-  ? process.env.AWS_SECRET_ACCESS_KEY : "";
-  export const PREFIX_TABLE = NODE_ENV === "production" 
-  ? "" : "-DEV";
+export const AWS_REGION = process.env.AWS_REGION
+  ? process.env.AWS_REGION
+  : "us-east-1";
+export const AWS_ACCESS_KEY = process.env.AWS_ACCESS_KEY
+  ? process.env.AWS_ACCESS_KEY
+  : "";
+export const AWS_SECRET_ACCESS_KEY = process.env.AWS_SECRET_ACCESS_KEY
+  ? process.env.AWS_SECRET_ACCESS_KEY
+  : "";
+export const PREFIX_TABLE = NODE_ENV === "production" ? "" : "-DEV";
+export const AWS_S3_ACCESS_KEY_ID = process.env.AWS_S3_ACCESS_KEY_ID
+  ? process.env.AWS_S3_ACCESS_KEY_ID
+  : "";
+export const AWS_S3_SECRET_ACCESS_KEY = process.env.AWS_S3_SECRET_ACCESS_KEY
+  ? process.env.AWS_S3_SECRET_ACCESS_KEY
+  : "";
+export const AWS_S3_BUCKET = process.env.AWS_S3_BUCKET
+  ? process.env.AWS_S3_BUCKET
+  : "";
---
+++ src/controllers/CallController.ts
@@ -0,0 +1,89 @@
+import { Request, Response } from "express";
+import AbstractController from "./AbstractController";
+import * as AWS from "aws-sdk";
+import {
+  AWS_S3_ACCESS_KEY_ID,
+  AWS_S3_SECRET_ACCESS_KEY,
+  AWS_REGION,
+  AWS_S3_BUCKET,
+} from "../config";
+import { isUndefined } from "util";
+
+class CallController extends AbstractController {
+  private static instance: CallController;
+  public static getInstance(): AbstractController {
+    if (this.instance) {
+      return this.instance;
+    }
+    this.instance = new CallController("call");
+    return this.instance;
+  }
+
+  protected initRoutes(): void {
+    this.router.get("/getCalls", this.getVideos.bind(this));
+    this.router.post("/uploadCall", this.postUploadVideo.bind(this));
+  }
+
+  private async getVideos(req: Request, res: Response) {
+    try {
+      AWS.config.update({
+        accessKeyId: AWS_S3_ACCESS_KEY_ID,
+        secretAccessKey: AWS_S3_SECRET_ACCESS_KEY,
+        region: AWS_REGION,
+      });
+
+      const { fileName, file } = req.body;
+
+      const params = {
+        Bucket: AWS_S3_BUCKET,
+        Key: fileName,
+        Body: file,
+      };
+
+      const s3 = new AWS.S3();
+
+      s3.listObjects(params, function (err, data) {
+        if (err) {
+          res.status(500).json({ error: err.message });
+        } else {
+          res.status(200).json({ data: data });
+        }
+      });
+    } catch (err: any) {
+      res.status(500).json({ error: err.message });
+    }
+  }
+
+  private async postUploadVideo(req: Request, res: Response) {
+    try {
+      AWS.config.update({
+        accessKeyId: AWS_S3_ACCESS_KEY_ID,
+        secretAccessKey: AWS_S3_SECRET_ACCESS_KEY,
+        region: AWS_REGION,
+      });
+
+      const fileName = req.body.fileName;
+      const blob = req.body.blob;
+
+      const params = {
+        Bucket: AWS_S3_BUCKET,
+        Key: fileName,
+        Body: blob,
+      };
+
+      const s3 = new AWS.S3();
+
+      s3.putObject(params, function (err: any, data: any) {
+        if (err) {
+          res.status(500).json({ error: err.message });
+        } else {
+          res.status(201).json({ message: data });
+        }
+      });
+    } catch (err: any) {
+      res.status(500).json({ error: err.message });
+    }
+  }
+}
+
+export default CallController;
