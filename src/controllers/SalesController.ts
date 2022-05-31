afbb0d12ff31d83f2881f1402985f62d16b98236
A01750400 <a01750400@tec.mx>
2022-05-31 12:42:19 -0500

    adjustments to SalesController

--- src/controllers/SalesController.ts
+++ src/controllers/SalesController.ts
@@ -1,7 +1,6 @@
 import { Request, Response } from "express";
 import AbstractController from "./AbstractController";
 import db from "../models";
-import { any } from "joi";
 
 class SalesContoller extends AbstractController {
   private static instance: SalesContoller;
@@ -24,8 +23,8 @@ class SalesContoller extends AbstractController {
 
   private async getOwnedProdcuts(req: Request, res: Response) {
     try {
-      let products = await db.sequelize.query(`SELECT p.product_id, p.product_name, p.product_description, p.price FROM Product as p, \`Category-Product\` as cp WHERE p.product_id = cp.product_id and cp.category_id = ${req.params.category_id} and p.product_id in (SELECT product_id FROM \`Order\` WHERE client_id = ${req.params.client_id})`);
-      res.status(200).send(products);
+      const products = await db.sequelize.query(`SELECT p.product_id, p.product_name, p.product_description, p.price FROM Product as p, \`Category-Product\` as cp WHERE p.product_id = cp.product_id and cp.category_id = ${req.params.category_id} and p.product_id in (SELECT product_id FROM \`Order\` WHERE client_id = ${req.params.client_id})`);
+      res.status(200).send(products[0]);
     } catch (error) {
       if (error instanceof Error) {
         res.status(500).send({ message: error.message });
@@ -37,8 +36,8 @@ class SalesContoller extends AbstractController {
 
   private async getProductsNotOwned(req: Request, res: Response) {
     try {
-      let products = await db.sequelize.query(`SELECT p.product_id, p.product_name, p.product_description, p.price FROM Product as p, \`Category-Product\` as cp WHERE p.product_id = cp.product_id and cp.category_id = ${req.params.category_id} and p.product_id not in (SELECT product_id FROM \`Order\` WHERE client_id = ${req.params.client_id})`);
-      res.status(200).send(products);
+      const products = await db.sequelize.query(`SELECT p.product_id, p.product_name, p.product_description, p.price FROM Product as p, \`Category-Product\` as cp WHERE p.product_id = cp.product_id and cp.category_id = ${req.params.category_id} and p.product_id not in (SELECT product_id FROM \`Order\` WHERE client_id = ${req.params.client_id})`);
+      res.status(200).send(products[0]);
     } catch (error) {
       if (error instanceof Error) {
         res.status(500).send({ message: error.message });
@@ -51,8 +50,14 @@ class SalesContoller extends AbstractController {
 
   private async getRecommendedProducts(req: Request, res: Response) {
     try {
-      let products = await db.sequelize.query(`SELECT p.product_id, p.product_name, p.product_description, p.price FROM Product as p, \`Category-Product\` as cp WHERE p.product_id = cp.product_id and cp.category_id = ${req.params.category_id} and p.product_id > all (SELECT o.product_id FROM \`Order\` as o, \`Category-Product\` as cp WHERE o.client_id = ${req.params.client_id} and cp.product_id = o.product_id and cp.category_id = ${req.params.category_id})`);
-      res.status(200).send(products);
+      if (Number(req.params.category_id) < 4) {
+        const products = await db.sequelize.query(`SELECT p.product_id, p.product_name, p.product_description, p.price FROM Product as p, \`Category-Product\` as cp WHERE p.product_id = cp.product_id and cp.category_id = ${req.params.category_id} and p.product_id > all (SELECT o.product_id FROM \`Order\` as o, \`Category-Product\` as cp WHERE o.client_id = ${req.params.client_id} and cp.product_id = o.product_id and cp.category_id = ${req.params.category_id})`);
+        res.status(200).send(products[0]);
+      }
+      else {
+        const products = await db.sequelize.query(`SELECT p.product_id, p.product_name, p.product_description, p.price FROM Product as p, \`Category-Product\` as cp WHERE p.product_id = cp.product_id and cp.category_id = ${req.params.category_id} and p.product_id not in (SELECT product_id FROM \`Order\` WHERE client_id = ${req.params.client_id})`);
+        res.status(200).send(products[0]);
+      }
     } catch (error) {
       if (error instanceof Error) {
         res.status(500).send({ message: error.message });
@@ -64,7 +69,7 @@ class SalesContoller extends AbstractController {
 
   private async getProductCategories(req: Request, res: Response) {
     try {
-      let productsCategory = await db["Product_category"].findAll({
+      const productsCategory = await db["Product_category"].findAll({
         attributes: ["category_name", "category_id"],
       });
       res.status(200).send(productsCategory);
@@ -79,7 +84,7 @@ class SalesContoller extends AbstractController {
 
   private async getProduct(req: Request, res: Response) {
     try {
-      let product = await db["Product"].findOne({
+      const product = await db["Product"].findOne({
         where: { product_id: req.params.product_id },
         attributes: [
           "product_id",
